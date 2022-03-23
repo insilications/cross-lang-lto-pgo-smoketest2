6a46167b40e290d9bae57f180228c51fd581b462
Francisco Boni Neto <boboniboni@gmail.com>
2022-03-23 17:49:37 -0300

    Small fixes & housekeeping.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
 // extern crate interesting;
-use core::ffi::c_void;
+// use core::ffi::c_void;
+//
+// #[link(name = "xyz", kind = "static")]
+// extern "C" {
+//     fn c_always_inlined() -> u32;
+//     fn c_never_inlined() -> u32;
+//     fn print_it() -> c_void;
+// }
 
-#[link(name = "xyz", kind = "static")]
-extern "C" {
-    fn c_always_inlined() -> u32;
-    fn c_never_inlined() -> u32;
-    fn print_it() -> c_void;
+#[no_mangle]
+#[inline(never)]
+pub fn caralho() {
+    println!("Hello Porra!");
 }
 
+
 fn main() {
-    let x = unsafe { c_always_inlined() + c_never_inlined() };
-    println!("blub: {}", x);
-    unsafe {
-        print_it();
-    }
+//     let x = unsafe { c_always_inlined() + c_never_inlined() };
+//     println!("blub: {}", x);
+//     unsafe {
+//         print_it();
+//     }
+    caralho();
 //     interesting::hello();
 //     interesting::hello();
 //     interesting::hello();
