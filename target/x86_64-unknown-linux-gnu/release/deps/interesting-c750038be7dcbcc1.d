e4710b9832a7cb1d54559bd3ce6b1131890c8ed0
Francisco Boni Neto <boboniboni@gmail.com>
2022-03-23 09:50:05 -0300

    Small fixes & housekeeping.

---
+++ target/x86_64-unknown-linux-gnu/release/deps/interesting-c750038be7dcbcc1.d
@@ -0,0 +1,7 @@
+/insilications/gstuff/stuff/things/build/git-clr/cross-lang-lto-pgo-smoketest2/target/x86_64-unknown-linux-gnu/release/deps/interesting-c750038be7dcbcc1.rmeta: crates/interesting/src/lib.rs
+
+/insilications/gstuff/stuff/things/build/git-clr/cross-lang-lto-pgo-smoketest2/target/x86_64-unknown-linux-gnu/release/deps/libinteresting-c750038be7dcbcc1.rlib: crates/interesting/src/lib.rs
+
+/insilications/gstuff/stuff/things/build/git-clr/cross-lang-lto-pgo-smoketest2/target/x86_64-unknown-linux-gnu/release/deps/interesting-c750038be7dcbcc1.d: crates/interesting/src/lib.rs
+
+crates/interesting/src/lib.rs:
---
+++ target/x86_64-unknown-linux-gnu/release/deps/rsmain-254013184a5fb6a7.d
@@ -0,0 +1,5 @@
+/insilications/gstuff/stuff/things/build/git-clr/cross-lang-lto-pgo-smoketest2/target/x86_64-unknown-linux-gnu/release/deps/rsmain-254013184a5fb6a7: src/main.rs
+
+/insilications/gstuff/stuff/things/build/git-clr/cross-lang-lto-pgo-smoketest2/target/x86_64-unknown-linux-gnu/release/deps/rsmain-254013184a5fb6a7.d: src/main.rs
+
+src/main.rs:
